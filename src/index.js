507e0f2c63303ff802de93d4f390227e85d9701d
MILAN BHADARKA <bhadarkamilan3@gmail.com>
2025-01-16 18:41:31 +0530

    initialize MoneyMate application with Express, MongoDB, and user model

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import { app } from "./app.js"
+import connectDb from "./db/index.js"
+import dotenv from 'dotenv'
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDb()
+.then( () => {
+    app.on("error", (err) => {
+        console.log("Express error! -> " + err);
+        throw err;
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port ->  + ${process.env.PORT}`);
+    })
+})
+.catch( (err) => {
+    console.log("MongoDb connection failed!!" + err);
+})
---
+++ src/models/user.model.js
@@ -0,0 +1,46 @@
+import mongoose, {Schema} from "mongoose";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt'
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required!"],
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required!"],
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required!"],
+    },
+    profilePicture: {
+        type: String,
+    }
+},{
+    timestamps: true
+})
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")){
+        return next;
+    }
+
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password,this.password);
+}
+
+
+
+export const User = mongoose.model("User", userSchema);
