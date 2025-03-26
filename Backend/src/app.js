0d68c631f8da1bf53c97acbd405a5aa077e67088
MILAN BHADARKA <bhadarkamilan3@gmail.com>
2025-03-26 22:37:24 +0530

    feat: update CORS configuration to allow specific origins

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,8 @@ const app = express();
 
 app.use(
     cors({
-        origin: process.env.CORS_ORIGIN, 
+        // origin: process.env.CORS_ORIGIN, 
+        origin: ["http://localhost:5173", "http://192.168.43.230:5173"], 
         credentials: true, 
         methods: ["GET", "POST", "PUT", "DELETE"], 
         allowedHeaders: ["Content-Type", "Authorization"], 
